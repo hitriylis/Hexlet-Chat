b4aaadc890a9c9e8858c844610717351c33ff88c
Andrei <avlugovov@gmail.com>
2023-09-23 17:04:09 +0400

    Fix rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,14 +1,13 @@
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
-// import { I18nextProvider, initReactI18next } from 'react-i18next';
-// import { Provider as StoreProvider } from 'react-redux';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { Provider as StoreProvider } from 'react-redux';
 import { Provider, ErrorBoundary } from '@rollbar/react';
-// import App from './components/App';
+import App from './components/App';
 import resources from './locales';
-// import AuthProvider from './contexts/AuthProvider';
-// import store from './slices';
-// import SocketProvider from './contexts/SocketProvider';
-// import FilterProvider from './contexts/FilterProvider';
+import AuthProvider from './contexts/AuthProvider';
+import store from './slices';
+import SocketProvider from './contexts/SocketProvider';
+import FilterProvider from './contexts/FilterProvider';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -25,26 +24,20 @@ const init = async () => {
     environment: 'testenv',
   };
 
-  function TestError() {
-    const a = null;
-    return a.hello();
-  }
-
   return (
-    // <StoreProvider store={store}>
-    //   <SocketProvider>
-    //     <AuthProvider>
-    //       <I18nextProvider i18n={i18n}>
-    //         <FilterProvider>
-    //           <App />
-    //         </FilterProvider>
-    //       </I18nextProvider>
-    //     </AuthProvider>
-    //   </SocketProvider>
-    // </StoreProvider>
     <Provider config={rollbarConfig}>
       <ErrorBoundary>
-        <TestError />
+        <StoreProvider store={store}>
+          <SocketProvider>
+            <AuthProvider>
+              <I18nextProvider i18n={i18n}>
+                <FilterProvider>
+                  <App />
+                </FilterProvider>
+              </I18nextProvider>
+            </AuthProvider>
+          </SocketProvider>
+        </StoreProvider>
       </ErrorBoundary>
     </Provider>
   );
