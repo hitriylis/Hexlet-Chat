688e9040474db65aa2cbefacbae0eb395f137f0f
Andrei <avlugovov@gmail.com>
2023-10-15 13:45:15 +0400

    Fix modal selectors

--- frontend/src/selectors/modal.js
+++ frontend/src/selectors/modal.js
@@ -4,6 +4,8 @@ const { actions } = modalSlice;
 
 const selectors = {
   selectModalType: (state) => state.modals.modalType,
+
+  selectOpened: (state) => state.modals.isOpened,
 };
 
 export { actions, selectors };
